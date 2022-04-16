e117e19fbe8509edae0970fcc0af380b693b1de5
Abdullah Al Safayet <abdullah.safayet11@gmail.com>
2022-04-17 01:32:46 +0600

    banner

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./components/Pages/Home/Home";
 import Footer from "./components/Shared/Footer/Footer";
 import Header from "./components/Shared/Header/Header";
 
@@ -6,6 +8,10 @@ function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>} />
+        <Route path="home" element={<Home></Home>} />
+      </Routes>
       <Footer></Footer>
     </div>
   );
--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
       </span>
       <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
-          <a href="#" class="mr-4 hover:underline md:mr-6 ">
+          <a href="#" class="mr-4 hover:underline md:mr-6 ">    
             Docs
           </a>
         </li>
