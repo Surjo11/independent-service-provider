6e33e400f0ac6dcee6c61184e2f38046d758371f
Abdullah Al Safayet <abdullah.safayet11@gmail.com>
2022-04-17 00:02:28 +0600

    footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Footer from "./components/Shared/Footer/Footer";
 import Header from "./components/Shared/Header/Header";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Footer></Footer>
     </div>
   );
 }
